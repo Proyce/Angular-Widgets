a4233546f446bff109fd406ac29c5345b0322625
Prosper Nnadi <prospernnadi3@gmail.com>
2021-01-15 14:18:47 +0100

    Generated component within the Elements module

--- src/app/elements/elements.module.ts
+++ src/app/elements/elements.module.ts
@@ -2,10 +2,11 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 
 import { ElementsRoutingModule } from './elements-routing.module';
+import { ElementsHomeComponent } from './elements-home/elements-home.component';
 
 
 @NgModule({
-  declarations: [],
+  declarations: [ElementsHomeComponent],
   imports: [
     CommonModule,
     ElementsRoutingModule
