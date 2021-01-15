2226c48b037f8aecdf3840df5ffbe189c7a92b65
Prosper Nnadi <prospernnadi3@gmail.com>
2021-01-15 15:10:40 +0100

    Generated component within the Collections module

--- src/app/collections/collections.module.ts
+++ src/app/collections/collections.module.ts
@@ -2,10 +2,11 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 
 import { CollectionsRoutingModule } from './collections-routing.module';
+import { CollectionsHomeComponent } from './collections-home/collections-home.component';
 
 
 @NgModule({
-  declarations: [],
+  declarations: [CollectionsHomeComponent],
   imports: [
     CommonModule,
     CollectionsRoutingModule
