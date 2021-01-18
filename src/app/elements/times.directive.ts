1bbe5701e149a1808bff495c366693a680f38a66
Prosper Nnadi <prospernnadi3@gmail.com>
2021-01-18 14:24:41 +0100

    Added Placeholder

--- src/app/elements/times.directive.ts
+++ src/app/elements/times.directive.ts
@@ -1,10 +1,21 @@
-import { Directive } from '@angular/core';
+import { Directive, TemplateRef, ViewContainerRef, Input } from '@angular/core';
 
 @Directive({
   selector: '[appTimes]'
 })
 export class TimesDirective {
 
-  constructor() { }
+  constructor(
+    private viewContainer: ViewContainerRef,
+    private templateRef: TemplateRef<any>
+  ) { }
+
+  @Input('appTimes') set render(times: number) {
+    this.viewContainer.clear();
+
+    for (let i = 0; i < times; i++) {
+      this.viewContainer.createEmbeddedView(this.templateRef, {});
+    }
+  }
 
 }
