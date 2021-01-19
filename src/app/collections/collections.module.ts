38bdfacff60e2ec12d00eb2d54eadf963df3db4f
Prosper Nnadi <prospernnadi3@gmail.com>
2021-01-19 11:40:48 +0100

    Displayed table componenet from shared module

--- src/app/collections/collections.module.ts
+++ src/app/collections/collections.module.ts
@@ -4,13 +4,15 @@ import { CommonModule } from '@angular/common';
 import { CollectionsRoutingModule } from './collections-routing.module';
 import { CollectionsHomeComponent } from './collections-home/collections-home.component';
 import { TableComponent } from './table/table.component';
+import {SharedModule} from '../shared/shared.module'
 
 
 @NgModule({
   declarations: [CollectionsHomeComponent, TableComponent],
   imports: [
     CommonModule,
-    CollectionsRoutingModule
+    CollectionsRoutingModule,
+    SharedModule
   ],
   // exports: [CollectionsHomeComponent]
 })
