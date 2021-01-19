07516bf5952c221f339c9aeabd751020a2c8c3c9
Prosper Nnadi <prospernnadi3@gmail.com>
2021-01-19 16:35:41 +0100

    Generated components for child nav routes, 122

--- src/app/collections/collections.module.ts
+++ src/app/collections/collections.module.ts
@@ -4,11 +4,14 @@ import { CommonModule } from '@angular/common';
 import { CollectionsRoutingModule } from './collections-routing.module';
 import { CollectionsHomeComponent } from './collections-home/collections-home.component';
 import { TableComponent } from './table/table.component';
-import {SharedModule} from '../shared/shared.module'
+import {SharedModule} from '../shared/shared.module';
+import { BiographyComponent } from './biography/biography.component';
+import { CompaniesComponent } from './companies/companies.component';
+import { PartnersComponent } from './partners/partners.component'
 
 
 @NgModule({
-  declarations: [CollectionsHomeComponent, TableComponent],
+  declarations: [CollectionsHomeComponent, TableComponent, BiographyComponent, CompaniesComponent, PartnersComponent],
   imports: [
     CommonModule,
     CollectionsRoutingModule,
