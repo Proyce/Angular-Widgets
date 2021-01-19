503b524713dad6d7ee34307fb5637cbcc1878e36
Prosper Nnadi <prospernnadi3@gmail.com>
2021-01-19 16:54:41 +0100

    Configs for child nav routes, 122

--- src/app/collections/collections-routing.module.ts
+++ src/app/collections/collections-routing.module.ts
@@ -1,9 +1,19 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import { CollectionsHomeComponent } from './collections-home/collections-home.component';
+import {BiographyComponent} from './biography/biography.component';
+import {CompaniesComponent} from './companies/companies.component';
+import {PartnersComponent} from './partners/partners.component';
 
 const routes: Routes = [
-  { path: '', component: CollectionsHomeComponent }
+{   path: '', 
+    component: CollectionsHomeComponent,
+    children: [
+      {path: '', component: BiographyComponent},
+      {path: 'companies', component: CompaniesComponent},
+      {path: 'partners', component: PartnersComponent},
+    ]
+}
 ];
 
 @NgModule({
