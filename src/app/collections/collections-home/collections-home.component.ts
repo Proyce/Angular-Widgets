8299084d197f9cad51ef45ecafc2b3ca2bd8b48a
Prosper Nnadi <prospernnadi3@gmail.com>
2021-01-19 15:44:38 +0100

    Didplayed table in the Collections page

--- src/app/collections/collections-home/collections-home.component.ts
+++ src/app/collections/collections-home/collections-home.component.ts
@@ -6,6 +6,27 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./collections-home.component.css']
 })
 export class CollectionsHomeComponent implements OnInit {
+  data = [
+    {name: 'James', age: 24, job: 'Designer', employed: true},
+    {name: 'Peter', age: 28, job: 'Developer', employed: false},
+    {name: 'Laura', age: 32, job: 'Marketer', employed: true},
+    {name: 'James', age: 24, job: 'Designer', employed: true},
+    {name: 'Peter', age: 28, job: 'Developer', employed: false},
+    {name: 'Laura', age: 32, job: 'Marketer', employed: true},
+    {name: 'James', age: 24, job: 'Designer', employed: true},
+    {name: 'Peter', age: 28, job: 'Developer', employed: false},
+    {name: 'Laura', age: 32, job: 'Marketer', employed: true},
+    {name: 'James', age: 24, job: 'Designer', employed: true},
+    {name: 'Peter', age: 28, job: 'Developer', employed: false},
+    {name: 'Laura', age: 32, job: 'Marketer', employed: true}
+  ];
+
+  headers = [
+  {key: 'employed', label: 'Has a Job?'},
+  {key: 'name', label: 'Name'},
+  {key: 'age', label: 'Age'},
+  {key: 'job', label: 'Job'}
+  ];
 
   constructor() { }
 
