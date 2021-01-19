63a1b2b69a303f4cccb5f11bc19cb3e20c1bbb28
Prosper Nnadi <prospernnadi3@gmail.com>
2021-01-19 23:22:43 +0100

    Completed Tabs Component

--- src/app/collections/collections.module.ts
+++ src/app/collections/collections.module.ts
@@ -7,11 +7,12 @@ import { TableComponent } from './table/table.component';
 import {SharedModule} from '../shared/shared.module';
 import { BiographyComponent } from './biography/biography.component';
 import { CompaniesComponent } from './companies/companies.component';
-import { PartnersComponent } from './partners/partners.component'
+import { PartnersComponent } from './partners/partners.component';
+import { TabsComponent } from './tabs/tabs.component'
 
 
 @NgModule({
-  declarations: [CollectionsHomeComponent, TableComponent, BiographyComponent, CompaniesComponent, PartnersComponent],
+  declarations: [CollectionsHomeComponent, TableComponent, BiographyComponent, CompaniesComponent, PartnersComponent, TabsComponent],
   imports: [
     CommonModule,
     CollectionsRoutingModule,
