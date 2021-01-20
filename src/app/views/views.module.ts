ff0bf1ae30b1fefe0acf9d8b3b1e7a78e1129099
Prosper Nnadi <prospernnadi3@gmail.com>
2021-01-20 10:35:10 +0100

    Generated ItemList Component

--- src/app/views/views.module.ts
+++ src/app/views/views.module.ts
@@ -4,10 +4,11 @@ import {SharedModule} from '../shared/shared.module';
 import { ViewsRoutingModule } from './views-routing.module';
 import { ViewsHomeComponent } from './views-home/views-home.component';
 import { StatisticsComponent } from './statistics/statistics.component';
+import { ItemListComponent } from './item-list/item-list.component';
 
 
 @NgModule({
-  declarations: [ViewsHomeComponent, StatisticsComponent],
+  declarations: [ViewsHomeComponent, StatisticsComponent, ItemListComponent],
   imports: [
     CommonModule,
     ViewsRoutingModule,
